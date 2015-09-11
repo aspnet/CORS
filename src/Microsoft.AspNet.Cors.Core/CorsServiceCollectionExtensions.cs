9ceb718d0e6dd30f3e2e8d92d3432549d78359ab
Kiran Challa <kichalla@microsoft.com>
2015-09-11 10:35:14 -0700

    [Fixes #33]AddCors should adopt new pattern

--- src/Microsoft.AspNet.Cors.Core/CorsServiceCollectionExtensions.cs
+++ src/Microsoft.AspNet.Cors.Core/CorsServiceCollectionExtensions.cs
@@ -16,29 +16,45 @@ namespace Microsoft.Framework.DependencyInjection
     public static class CorsServiceCollectionExtensions
     {
         /// <summary>
-        /// Can be used to configure services in the <paramref name="serviceCollection"/>.
-        /// </summary>
-        /// <param name="serviceCollection">The service collection which needs to be configured.</param>
-        /// <param name="configure">A delegate which is run to configure the services.</param>
-        /// <returns></returns>
-        public static IServiceCollection ConfigureCors(
-            [NotNull] this IServiceCollection serviceCollection,
-            [NotNull] Action<CorsOptions> configure)
-        {
-            return serviceCollection.Configure(configure);
-        }
-
-        /// <summary>
         /// Add services needed to support CORS to the given <paramref name="serviceCollection"/>.
         /// </summary>
         /// <param name="serviceCollection">The service collection to which CORS services are added.</param>
         /// <returns>The updated <see cref="IServiceCollection"/>.</returns>
         public static IServiceCollection AddCors(this IServiceCollection serviceCollection)
         {
+            if (serviceCollection == null)
+            {
+                throw new ArgumentNullException(nameof(serviceCollection));
+            }
+
             serviceCollection.AddOptions();
             serviceCollection.TryAdd(ServiceDescriptor.Transient<ICorsService, CorsService>());
             serviceCollection.TryAdd(ServiceDescriptor.Transient<ICorsPolicyProvider, DefaultCorsPolicyProvider>());
             return serviceCollection;
+        }
+
+        /// <summary>
+        /// Add services needed to support CORS to the given <paramref name="serviceCollection"/>.
+        /// </summary>
+        /// <param name="serviceCollection">The service collection to which CORS services are added.</param>
+        /// <param name="configure">A delegate which is run to configure the services.</param>
+        /// <returns>The updated <see cref="IServiceCollection"/>.</returns>
+        public static IServiceCollection AddCors(
+            this IServiceCollection serviceCollection,
+            Action<CorsOptions> configure)
+        {
+            if (serviceCollection == null)
+            {
+                throw new ArgumentNullException(nameof(serviceCollection));
+            }
+
+            if (configure == null)
+            {
+                throw new ArgumentNullException(nameof(configure));
+            }
+
+            serviceCollection.Configure(configure);
+            return serviceCollection.AddCors();
         }
     }
 }
